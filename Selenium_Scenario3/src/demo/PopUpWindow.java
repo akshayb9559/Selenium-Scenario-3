555ec2528f424857ad3846ab4bb8edf7f8fecb1b
Akshay Bramhane <44193607+akshayb9559@users.noreply.github.com>
2020-10-22 22:19:29 +0530

    Update PopUpWindow.java

--- Selenium_Scenario3/src/demo/PopUpWindow.java
+++ Selenium_Scenario3/src/demo/PopUpWindow.java
@@ -1,11 +1,14 @@
 package demo;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 
 public class PopUpWindow {
 
@@ -14,21 +17,40 @@ public class PopUpWindow {
 
 	@Before
 	public void setUp() throws Exception {
+
 		System.setProperty("webdriver.chrome.driver", "D:\\Certification Data\\Selenium\\Driver\\chromedriver.exe");
 		wd = new ChromeDriver();
-		wd.get(url);
 		wd.manage().window().maximize();
+		wd.get(url);
+		Thread.sleep(3000);
 	}
 
 	@Test
 	public void test() {
 
-		
-		
+		wd.findElement(By.linkText("Good PopUp #1")).click();
+
+		Set<String> handle = wd.getWindowHandles();
+
+		Iterator<String> str = handle.iterator();
+
+		String parentWindow = str.next();
+		String childWindow = str.next();
+
+		wd.switchTo().window(childWindow);
+
+		System.out.println("Child Window Title is : " + wd.getTitle());
+		wd.close();
+
+		wd.switchTo().window(parentWindow);
+		System.out.println("Parent Window Title is : " + wd.getTitle());
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
+
 		wd.quit();
 	}
+
 }
